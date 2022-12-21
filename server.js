7ccf8853ecb2443c88a69528a76d1e5ea7e672fe
Hieu IT <hieuxuan213@gmail.com>
2022-12-21 08:41:10 +0700

    video5

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
+app.get('/', (req, res) => {
+    res.send('Hỏi dân IT')
+})
+app.get('/about', (req, res) => {
+    res.send('about chao')
 })
 
-server.listen(8080, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
